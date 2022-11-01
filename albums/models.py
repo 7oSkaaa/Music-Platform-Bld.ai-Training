0ead850dec315b8c75b2e6f6aecc2c7285442eec
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-11-01 13:27:39 +0200

    edit: change camel case to snake case

--- albums/models.py
+++ albums/models.py
@@ -5,8 +5,8 @@ from artists.models import Artist
 class Album(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, null=False, related_name='albums')
     name = models.CharField(default='New Album', max_length=100, blank=True)
-    creationDate = models.DateTimeField(auto_now_add=True)
-    releaseDate = models.DateTimeField(null=False, blank=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    release_date = models.DateTimeField(null=False, blank=False)
     cost = models.DecimalField(decimal_places=2, max_digits=10, blank=False, null=False)
     
     def __str__(self):
--- artists/models.py
+++ artists/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Artist(models.Model):
-    stageName = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    socialLink = models.URLField(max_length=200, blank=True, null=False)
+    stage_name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    social_link = models.URLField(max_length=200, blank=True, null=False)
     
     class Meta:
         # This is the default ordering for the model
-        ordering = ['stageName']
+        ordering = ['stage_name']
         
         
     def __str__(self):
         # This is the default string representation of the model
-        return self.stageName
+        return self.stage_name
